0d68898cab853dda6092485704e404d2e5326a22
vincenzomartusciello <vinz.mart@gmail.com>
2019-12-08 17:49:14 +0100

    Added background to the game window and check for hero if is visible
    Signed-off-by: Vincenzo Martusciello <vinz.mart@gmail.com>

--- Game/src/main/java/it/unisa/theneverendingrun/GameEngine.java
+++ Game/src/main/java/it/unisa/theneverendingrun/GameEngine.java
@@ -1,19 +1,39 @@
 package it.unisa.theneverendingrun;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import it.unisa.theneverendingrun.models.Sprite;
+import it.unisa.theneverendingrun.models.background.AbstractScrollingBackground;
+import it.unisa.theneverendingrun.models.services.ForestFactory;
 import org.mini2Dx.core.game.BasicGame;
 import org.mini2Dx.core.graphics.Graphics;
 
 
 public class GameEngine extends BasicGame {
 
-	public static final String GAME_IDENTIFIER = "it.unisa.theneverendingrun";
+	static final String GAME_IDENTIFIER = "it.unisa.theneverendingrun";
+
+    private SpriteBatch spriteBatch;
+    private AbstractScrollingBackground background;
+
+    //TODO: change with the real hero
+    private Sprite hero;
 
     @Override
     public void initialise() {
+        spriteBatch = new SpriteBatch();
+        var factory = new ForestFactory();
+        background = factory.createBackground();
     }
 
     @Override
     public void update(float delta) {
+        background.scroll();
+
+        //TODO: Now return NullPointerException because the hero is not real.
+        if (!hero.isXAxisVisible(Gdx.graphics.getWidth())) {
+            initialise();
+        }
     }
 
     @Override
@@ -22,5 +42,8 @@ public class GameEngine extends BasicGame {
 
     @Override
     public void render(Graphics g) {
+        spriteBatch.begin();
+        spriteBatch.draw(background,0,0);
+        spriteBatch.end();
     }
 }
