24d98bbe33f3b7a5b9e5c83a470057cc8278f54e
vincenzomartusciello <vinz.mart@gamil.com>
2019-12-08 18:43:37 +0100

    Added SlidableObstacle
    Signed-off-by: Vincenzo Martusciello <vinz.mart@gmail.com>

--- Game/src/main/java/it/unisa/theneverendingrun/models/obstacles/SlidableObstacle.java
+++ Game/src/main/java/it/unisa/theneverendingrun/models/obstacles/SlidableObstacle.java
@@ -1,46 +1,32 @@
 package it.unisa.theneverendingrun.models.obstacles;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.math.Rectangle;
 
 import java.util.concurrent.ThreadLocalRandom;
 
 public class SlidableObstacle extends AbstractObstacle {
 
-    private final int MAX_SLIDE_DISTANCE;
-    private int width;
-    private int height;
-    private Rectangle rectBound;
+    private float maxSlideDistance;
 
-    public SlidableObstacle(Texture texture, int srcX, int srcY) {
+    public SlidableObstacle(Texture texture, int srcX, int srcY, float maxSlideDistance) {
         super(texture, srcX, srcY);
-        generateDimensions();
+        setMaxSlideDistance(maxSlideDistance);
+
     }
 
-/*
+    private void setMaxSlideDistance(float maxSlideDistance) {
+        this.maxSlideDistance = maxSlideDistance;
+    }
 
-    public SlidableObstacle(int STANDARD_HEIGHT, int MAX_SLIDE_DISTANCE, float posX, float posY) {
-        super(STANDARD_HEIGHT, posX, posY);
-        this.MAX_SLIDE_DISTANCE = MAX_SLIDE_DISTANCE;
-        this.generateDimensions();
-    }*/
 
     @Override
     public void generateDimensions() {
-        int randomDistance = ThreadLocalRandom.current().nextInt(super.getSTANDARD_HEIGHT(), MAX_SLIDE_DISTANCE + 1);
-        randomDistance = (randomDistance % super.getSTANDARD_HEIGHT()) * super.getSTANDARD_HEIGHT();
-        width = randomDistance;
-        height = super.getSTANDARD_HEIGHT();
-    }
+        var maxGap = maxSlideDistance - 2;
+        var minGap = (maxSlideDistance / 2) - 2;
 
-    @Override
-    public Rectangle getHitBox() {
-        rectBound = new Rectangle();
-        rectBound.setX((float) super.getComponentX());
-        rectBound.setY((float) super.getComponentY());
-        rectBound.height = (float) height;
-        rectBound.width = (float) width;
-        return rectBound;
+        var dimension = (float)ThreadLocalRandom.current().nextDouble(maxGap - minGap);
+        setSize(dimension, Gdx.graphics.getHeight() - getY());
     }
 
 }
