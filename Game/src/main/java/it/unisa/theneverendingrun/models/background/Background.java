18b81349c56f045e0c96c26140b497e397ed3894
LuigiTer <luigi.terrone97@gmail.com>
2019-12-08 17:30:49 +0100

    Added javadoc for all the methods in background.java
    Signed-off-by: LuigiTer <luigi.terrone97@gmail.com>

--- Game/src/main/java/it/unisa/theneverendingrun/models/background/Background.java
+++ Game/src/main/java/it/unisa/theneverendingrun/models/background/Background.java
@@ -5,27 +5,57 @@ import it.unisa.theneverendingrun.models.Sprite;
 
 public class Background extends Sprite {
 
+    /**
+     * Store the background width
+     */
     private int originBackgroundWidth;
+
+    /**
+     * Store the background height
+     */
     private int originBackgroundHeight;
 
+    /**
+     * Background constructor. Set the initial height and width
+     *
+     * @param texture   texture of the background
+     * @param srcWidth  source width of the background
+     * @param srcHeight source height of the background
+     */
     public Background(Texture texture, int srcWidth, int srcHeight) {
         super(texture, srcWidth, srcHeight);
         setOriginBackgroundWidth(texture.getWidth());
         setOriginBackgroundHeight(texture.getHeight());
     }
 
+    /**
+     * Background width getter
+     */
     public int getOriginBackgroundWidth() {
         return originBackgroundWidth;
     }
 
+    /**
+     * Background width setter
+     *
+     * @param originBackgroundWidth width to set
+     */
     public void setOriginBackgroundWidth(int originBackgroundWidth) {
         this.originBackgroundWidth = originBackgroundWidth;
     }
 
+    /**
+     * Background height getter
+     */
     public int getOriginBackgroundHeight() {
         return originBackgroundHeight;
     }
 
+    /**
+     * Background height setter
+     *
+     * @param originBackgroundHeight height to set
+     */
     public void setOriginBackgroundHeight(int originBackgroundHeight) {
         this.originBackgroundHeight = originBackgroundHeight;
     }
