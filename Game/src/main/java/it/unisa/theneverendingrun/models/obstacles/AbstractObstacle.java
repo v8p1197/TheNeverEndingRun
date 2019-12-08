a3174632f6988804febc7a2f299d810b5c911ba6
vincenzomartusciello <vinz.mart@gmail.com>
2019-12-08 19:08:40 +0100

    Fixed SlidableObstacle Height Fixed AbstractObstacle setSize()
    Signed-off-by: Vincenzo Martusciello <vinz.mart@gmail.com>

--- Game/src/main/java/it/unisa/theneverendingrun/models/obstacles/AbstractObstacle.java
+++ Game/src/main/java/it/unisa/theneverendingrun/models/obstacles/AbstractObstacle.java
@@ -5,12 +5,21 @@ import it.unisa.theneverendingrun.models.Sprite;
 
 public abstract class AbstractObstacle extends Sprite {
 
+    private boolean isDimensionGenerated = false;
+
     AbstractObstacle(Texture texture, int srcX, int srcY) {
         super(texture);
         setX(srcX);
         setY(srcY);
         generateDimensions();
+        isDimensionGenerated = true;
     }
 
-    public abstract void generateDimensions();
+    abstract void generateDimensions();
+
+    @Override
+    public void setSize(float width, float height) {
+        if (!isDimensionGenerated)
+            super.setSize(width, height);
+    }
 }
--- Game/src/main/java/it/unisa/theneverendingrun/models/obstacles/SlidableObstacle.java
+++ Game/src/main/java/it/unisa/theneverendingrun/models/obstacles/SlidableObstacle.java
@@ -1,6 +1,5 @@
 package it.unisa.theneverendingrun.models.obstacles;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 
 import java.util.concurrent.ThreadLocalRandom;
@@ -8,25 +7,38 @@ import java.util.concurrent.ThreadLocalRandom;
 public class SlidableObstacle extends AbstractObstacle {
 
     private float maxSlideDistance;
+    private float maxJumpDistance;
 
-    public SlidableObstacle(Texture texture, int srcX, int srcY, float maxSlideDistance) {
+    public SlidableObstacle(Texture texture, int srcX, int srcY, float maxSlideDistance, float maxJumpDistance) {
         super(texture, srcX, srcY);
         setMaxSlideDistance(maxSlideDistance);
+        setMaxJumpDistance(maxJumpDistance);
+    }
 
+    private float getMaxJumpDistance() {
+        return this.maxJumpDistance;
+    }
+
+    private float getMaxSlideDistance() {
+        return this.maxSlideDistance;
     }
 
     private void setMaxSlideDistance(float maxSlideDistance) {
         this.maxSlideDistance = maxSlideDistance;
     }
 
+    private void setMaxJumpDistance(float maxJumpDistance) {
+        this.maxJumpDistance = maxJumpDistance;
+    }
+
 
     @Override
     public void generateDimensions() {
-        var maxGap = maxSlideDistance - 2;
-        var minGap = (maxSlideDistance / 2) - 2;
+        var maxGap = getMaxSlideDistance() - 2;
+        var minGap = (getMaxSlideDistance() / 2) - 2;
 
         var dimension = (float)ThreadLocalRandom.current().nextDouble(maxGap - minGap);
-        setSize(dimension, Gdx.graphics.getHeight() - getY());
+        setSize(dimension, getMaxJumpDistance());
     }
 
 }
