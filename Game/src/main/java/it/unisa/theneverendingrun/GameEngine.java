11181c4a9dab8dfbf7029ddfbb28922877c60a1b
vbaldi7 <v.baldi7@studenti.unisa.it>
2019-12-09 19:02:52 +0100

    Added constant PG_VELOCITY in GameEngine class for setting the pg velocity for the animation. Added counter variable changeFrame in GameEngine class in order to change the frame in function of PG_VELOCITY.

--- Game/src/main/java/it/unisa/theneverendingrun/GameEngine.java
+++ Game/src/main/java/it/unisa/theneverendingrun/GameEngine.java
@@ -1,30 +1,46 @@
 package it.unisa.theneverendingrun;
 
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import it.unisa.theneverendingrun.factory.ForestFactory;
 import it.unisa.theneverendingrun.factory.RunFactory;
 import it.unisa.theneverendingrun.models.hero.Hero;
 import org.mini2Dx.core.game.BasicGame;
 import org.mini2Dx.core.graphics.Graphics;
-import org.mini2Dx.core.graphics.Sprite;
 
 
 public class GameEngine extends BasicGame {
 
-    public static final String GAME_IDENTIFIER = "it.unisa.theneverendingrun";
+	public static final String GAME_IDENTIFIER = "it.unisa.theneverendingrun";
     private HandlingInput input;
-
-    private Hero hero;
-
+	private static final int PG_VELOCITY = 3;
+    private SpriteBatch spriteBatch;
+	private RunFactory factory;
+	private Hero hero;
+	private int changeFrame;
 
     @Override
     public void initialise() {
         input = new HandlingInput();
+        spriteBatch = new SpriteBatch();
+        factory = new ForestFactory();
+        hero = factory.createHero();
+        changeFrame = 0;
     }
 
     @Override
     public void update(float delta) {
         input.getKeyWASD(hero);
+        if(changeFrame == PG_VELOCITY) {
+            hero.move();
+            changeFrame = 0;
+        }
+
+        if (hero.isLeft())
+            System.out.println("left");
+        if (hero.isRight())
+            System.out.println("right");
+        changeFrame++;
+        System.out.println(changeFrame);
     }
 
     @Override
@@ -33,5 +49,12 @@ public class GameEngine extends BasicGame {
 
     @Override
     public void render(Graphics g) {
+        spriteBatch.begin();
+        drawHero();
+        spriteBatch.end();
+    }
+
+    private void drawHero() {
+        hero.getSprite().draw(spriteBatch);
     }
 }
