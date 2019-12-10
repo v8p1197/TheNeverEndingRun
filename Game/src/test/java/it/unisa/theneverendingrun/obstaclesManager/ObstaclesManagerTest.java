a0d0cf9c7f8d57deef8dd3d0530efe1eab3dd576
andrewvali <58277048+andrewvali@users.noreply.github.com>
2019-12-10 16:51:38 +0100

    Added new tests to check the correct positioning and the correct type of new obstacles.
    Signed-off-by: giorge1 <g.ammendola4@studenti.unisa.it>

--- Game/src/test/java/it/unisa/theneverendingrun/obstaclesManager/ObstaclesManagerTest.java
+++ Game/src/test/java/it/unisa/theneverendingrun/obstaclesManager/ObstaclesManagerTest.java
@@ -1,15 +1,9 @@
 package it.unisa.theneverendingrun.obstaclesManager;
 
 import de.tomgrill.gdxtesting.GdxTestRunner;
-import it.unisa.theneverendingrun.Launcher;
-import it.unisa.theneverendingrun.models.obstacles.AbstractObstacle;
-import it.unisa.theneverendingrun.models.obstacles.JumpableObstacle;
-import it.unisa.theneverendingrun.models.obstacles.ObstacleType;
+import it.unisa.theneverendingrun.models.obstacles.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.util.LinkedList;
-import java.util.Random;
 
 import static org.junit.Assert.*;
 
@@ -17,9 +11,7 @@ import static org.junit.Assert.*;
 
 public class ObstaclesManagerTest {
 
-    private Random random = new Random(666);
     private static int OFFSET_MEASURE = 100;
-    private static int DEFAULT_MEASURE = 100;
 
     //random distances
     private float maxJumpingHeight = OFFSET_MEASURE * 3;
@@ -43,17 +35,16 @@ public class ObstaclesManagerTest {
 
     @Test
     public void testAddJumpableAfterJumpable() {
-        Launcher.main(new String[0]);
-        //running main to open the main window. this is needed in order to get the screen width
 
         ObstaclesManager obstaclesManager = new ObstaclesManager(maxJumpingHeight, standingHeight, maxSlidingDistance, slidingHeight, standingWidth);
 
         //add a first obstacle. since we want to test the case in which the last obstacle is jumpable,
         // get new obstacle until the one returned is jumpable
         AbstractObstacle firstObstacle = obstaclesManager.getNewAppropriateObstacle();
+        firstObstacle = obstaclesManager.getNewAppropriateObstacle();
         while (!(firstObstacle instanceof JumpableObstacle)) {
-            //move the last obstacle to generate another, otherwise it will be null
-            obstaclesManager.getObstacles().getLast().setPosition(0, 0);
+            //remove the last obstacle
+            obstaclesManager.getObstacles().removeLast();
             firstObstacle = obstaclesManager.getNewAppropriateObstacle();
         }
 
@@ -63,12 +54,43 @@ public class ObstaclesManagerTest {
         //the new obstacle type will be null or jumpable
         ObstacleType newType = obstaclesManager.getAppropriateObstacleType();
         assertTrue(newType == null || newType == ObstacleType.Jumpable);
+
+    }
+
+    @Test
+    public void testSlidableAfterJumpable() {
+
+        ObstaclesManager obstaclesManager = new ObstaclesManager(maxJumpingHeight, standingHeight, maxSlidingDistance, slidingHeight, standingWidth);
+
+        //add a first obstacle. since we want to test the case in which the last obstacle is jumpable,
+        // get new obstacle until the one returned is jumpable
+        AbstractObstacle firstObstacle = obstaclesManager.getNewAppropriateObstacle();
+        firstObstacle = obstaclesManager.getNewAppropriateObstacle();
+        while (!(firstObstacle instanceof JumpableObstacle)) {
+            //remove the last obstacle
+            obstaclesManager.getObstacles().removeLast();
+            firstObstacle = obstaclesManager.getNewAppropriateObstacle();
+        }
+
+        //move the rigth edge of the obstacle to the right edge of the window
+        firstObstacle.setPosition(firstObstacle.getX() - firstObstacle.getWidth() + 1, firstObstacle.getY());
+
+        //the new obstacle type will be null or slidable
+        ObstacleType newType = obstaclesManager.getAppropriateObstacleType();
+        assertTrue(newType == null || newType == ObstacleType.Slidable);
+
+        AbstractObstacle secondObstacle = obstaclesManager.getNewAppropriateObstacle();
+        while (!(secondObstacle instanceof SlidableObstacle)) {
+            //remove the last obstacle
+            obstaclesManager.getObstacles().removeLast();
+            secondObstacle = obstaclesManager.getNewAppropriateObstacle();
+        }
+        assertTrue(secondObstacle.getY() >= firstObstacle.getY() + firstObstacle.getHeight() + slidingHeight);
+
     }
 
     @Test
     public void notEnoughSpaceTest() {
-        Launcher.main(new String[0]);
-        //running main to open the main window. this is needed in order to get the screen width
         ObstaclesManager obstaclesManager = new ObstaclesManager(maxJumpingHeight, standingHeight, maxSlidingDistance, slidingHeight, standingWidth);
         //add a first obstacle. since we want to test the case in which the last obstacle is jumpable,
         // get new obstacle until the one returned is jumpable
@@ -83,8 +105,7 @@ public class ObstaclesManagerTest {
 
     @Test
     public void enoughSpaceTest() {
-        Launcher.main(new String[0]);
-        //running main to open the main window. this is needed in order to get the screen width
+
         ObstaclesManager obstaclesManager = new ObstaclesManager(maxJumpingHeight, standingHeight, maxSlidingDistance, slidingHeight, standingWidth);
         //add a first obstacle. since we want to test the case in which the last obstacle is jumpable,
         // get new obstacle until the one returned is jumpable
@@ -101,24 +122,44 @@ public class ObstaclesManagerTest {
     }
 
     @Test
-    public void getAppropriateObstacleType() {
+    public void setPositionJumpableTest() {
+
+        ObstacleFactory obstacleFactory = new ObstacleFactory(maxJumpingHeight, slidingHeight, standingWidth);
+        AbstractObstacle obs = obstacleFactory.getObstacle(ObstacleType.Jumpable, 0, 0);
 
         ObstaclesManager obstaclesManager = new ObstaclesManager(maxJumpingHeight, standingHeight, maxSlidingDistance, slidingHeight, standingWidth);
-        LinkedList<AbstractObstacle> obstacles = obstaclesManager.getObstacles();
-        obstaclesManager.getAppropriateObstacleType();
+        obstaclesManager.setPosition(obs);
 
-        if (obstacles.isEmpty()) {
-            assertNotNull(obstaclesManager.getAppropriateObstacleType());
-        }
-
+        //now we have an obstacle positioned at a certain height, we must check that it is jumpable
+        assertTrue(obs.getY() + obs.getHeight() + ObstaclesManager.OFFSET <= maxJumpingHeight);
     }
 
     @Test
-    public void setPosition() {
+    public void setPositionSlidableTest() {
+
+        ObstacleFactory obstacleFactory = new ObstacleFactory(maxJumpingHeight, slidingHeight, standingWidth);
+        AbstractObstacle obs = obstacleFactory.getObstacle(ObstacleType.Slidable, 0, 0);
+
+        ObstaclesManager obstaclesManager = new ObstaclesManager(maxJumpingHeight, standingHeight, maxSlidingDistance, slidingHeight, standingWidth);
+        obstaclesManager.setPosition(obs);
+
+        //now we have an obstacle positioned at a certain height, we must check that it is slidable
+        assertTrue(obs.getY() + ObstaclesManager.OFFSET >= slidingHeight);
     }
 
     @Test
-    public void clearOldObstacles() {
+    public void setPositionJumpableSlidableTest() {
 
+        ObstacleFactory obstacleFactory = new ObstacleFactory(maxJumpingHeight, slidingHeight, standingWidth);
+        AbstractObstacle obs = obstacleFactory.getObstacle(ObstacleType.JumpableSlidable, 0, 0);
+
+        ObstaclesManager obstaclesManager = new ObstaclesManager(maxJumpingHeight, standingHeight, maxSlidingDistance, slidingHeight, standingWidth);
+        obstaclesManager.setPosition(obs);
+
+        //now we have an obstacle positioned at a certain height, we must check that it is slidable
+        assertTrue((obs.getY() + obs.getHeight() + ObstaclesManager.OFFSET <= maxJumpingHeight) || (obs.getY() + ObstaclesManager.OFFSET >= slidingHeight));
     }
+
+
+
 }
