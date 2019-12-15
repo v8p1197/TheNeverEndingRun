0397176c60014b7c89f40be2772cc1dde4e05ec6
andrewvali <58277048+andrewvali@users.noreply.github.com>
2019-12-15 13:08:22 +0100

    Add class Enemy that extends Sprite

---
+++ Game/src/main/java/it/unisa/theneverendingrun/models/enemy/EnemyJumpable.java
@@ -0,0 +1,51 @@
+package it.unisa.theneverendingrun.models.enemy;
+
+import com.badlogic.gdx.graphics.Texture;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class EnemyJumpable extends Enemy {
+
+    private double maxJumpHeight;
+
+    private double maxWidth;
+
+    public EnemyJumpable(Texture texture, int srcX, int srcY, double maxJumpHeight, double maxWidth) {
+        super(texture, srcX, srcY);
+        setMaxJumpHeight(maxJumpHeight);
+        setMaxWidth(maxWidth);
+        generateDimensions();
+    }
+
+    public double getMaxJumpHeight() {
+        return maxJumpHeight;
+    }
+
+    public void setMaxJumpHeight(double maxJumpHeight) {
+        this.maxJumpHeight = maxJumpHeight;
+    }
+
+    public double getMaxWidth() {
+        return maxWidth;
+    }
+
+    public void setMaxWidth(double maxWidth) {
+        this.maxWidth = maxWidth;
+    }
+
+    /**
+     * Generate randomly the dimension of the jumpable obstacles given min max gap
+     */
+    @Override
+    public void generateDimensions() {
+        var maxGapHeight = getMaxJumpHeight() - 4;
+        var minGapHeight = getMaxJumpHeight() * 0.5;
+        var maxGapWidth = getMaxWidth() - 2;
+        var minGapWidth = getMaxWidth() / 2;
+
+        var randomHeight = (float) ThreadLocalRandom.current().nextDouble(minGapHeight, maxGapHeight);
+        var randomWidth = (float) ThreadLocalRandom.current().nextDouble(minGapWidth, maxGapWidth);
+        setSize(randomWidth, randomHeight);
+    }
+
+}
