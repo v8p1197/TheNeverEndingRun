c30ac529f56841f312495182d3996446c933ee96
v8p1197 <v.petrone6@studenti.unisa.it>
2019-12-18 15:08:18 +0100

    Added the spawnable abstract class and the spawnable types. Obstacles and enemies will implement it.
    Signed-off-by: giorge1 <g.ammendola4@studenti.unisa.it>

---
+++ Game/src/main/java/it/unisa/theneverendingrun/models/Spawnable.java
@@ -0,0 +1,13 @@
+package it.unisa.theneverendingrun.models;
+
+import com.badlogic.gdx.graphics.Texture;
+import it.unisa.theneverendingrun.models.hero.Hero;
+
+public abstract class Spawnable extends Sprite {
+
+    public Spawnable(Texture texture) {
+        super(texture);
+    }
+
+    public abstract void reactToCollision(Hero hero);
+}
