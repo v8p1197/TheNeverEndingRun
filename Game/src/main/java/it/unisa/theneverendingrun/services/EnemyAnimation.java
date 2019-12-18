2907d6912780cdfbe835eae019564c68beb79200
vbaldi7 <v.baldi7@studenti.unisa.it>
2019-12-18 10:05:59 +0100

    Added EnemyAnimation using strategy pattern for the animation of the enemy

---
+++ Game/src/main/java/it/unisa/theneverendingrun/services/EnemyAnimation.java
@@ -0,0 +1,10 @@
+package it.unisa.theneverendingrun.services;
+
+
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public interface EnemyAnimation {
+
+    Animation<TextureRegion> setAnimation();
+}
