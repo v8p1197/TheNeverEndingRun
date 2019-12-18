eb91a58ebd57d0b372571fdfc8995e342cd391fc
vbaldi7 <v.baldi7@studenti.unisa.it>
2019-12-18 15:13:24 +0100

    Minor performance improvement
    Signed-off-by: giorge1 <g.ammendola4@studenti.unisa.it>

--- Game/src/main/java/it/unisa/theneverendingrun/models/enemy/EnemyAttack.java
+++ Game/src/main/java/it/unisa/theneverendingrun/models/enemy/EnemyAttack.java
@@ -7,13 +7,16 @@ import it.unisa.theneverendingrun.services.EnemyAnimation;
 
 public class EnemyAttack implements EnemyAnimation {
 
+    private static TextureRegion[] tVect = new com.badlogic.gdx.graphics.g2d.TextureRegion[10];
+
     @Override
     public Animation setAnimation() {
-        var tVect = new com.badlogic.gdx.graphics.g2d.TextureRegion[10];
-        for (int i = 1; i <= 10; i++) {
-            TextureRegion t = new TextureRegion(new Texture("images/enemy/attack/wolf_attack_" + i + ".png"));
-            tVect[i - 1] = t;
-        }
+        if (tVect[0] == null)
+            for (int i = 1; i <= 10; i++) {
+                TextureRegion t = new TextureRegion(new Texture("images/enemy/wolf/attack/wolf_attack_" + i + ".png"));
+                tVect[i - 1] = t;
+            }
         return new Animation(0.05f, tVect);
+
     }
 }
--- Game/src/main/java/it/unisa/theneverendingrun/models/enemy/EnemyIdle.java
+++ Game/src/main/java/it/unisa/theneverendingrun/models/enemy/EnemyIdle.java
@@ -2,16 +2,18 @@ package it.unisa.theneverendingrun.models.enemy;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
-import it.unisa.theneverendingrun.services.EnemyAnimation;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import it.unisa.theneverendingrun.services.EnemyAnimation;
 
 public class EnemyIdle implements EnemyAnimation {
 
+    private static TextureRegion[] tVect = new TextureRegion[12];
+
     @Override
     public Animation setAnimation() {
-        var tVect = new com.badlogic.gdx.graphics.g2d.TextureRegion[12];
+        if (tVect[0] == null)
         for (int i = 1; i <= 12; i++) {
-            TextureRegion t = new TextureRegion(new Texture("images/enemy/idle/wolf_idle_" + i + ".png"));
+            TextureRegion t = new TextureRegion(new Texture("images/enemy/wolf/idle/wolf_idle_" + i + ".png"));
             tVect[i - 1] = t;
         }
         return new Animation(0.05f, tVect);
