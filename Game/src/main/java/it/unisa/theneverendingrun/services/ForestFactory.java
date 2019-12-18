17cdd76f70e6aaf2bc64c85d557e1e26b8f05573
giorge1 <g.ammendola4@studenti.unisa.it>
2019-12-18 15:18:39 +0100

    Moved the Obstacle and Enemy factories into Forest factory
    Signed-off-by: giorge1 <g.ammendola4@studenti.unisa.it>

--- Game/src/main/java/it/unisa/theneverendingrun/services/ForestFactory.java
+++ Game/src/main/java/it/unisa/theneverendingrun/services/ForestFactory.java
@@ -1,10 +1,15 @@
 package it.unisa.theneverendingrun.services;
 
 import com.badlogic.gdx.Gdx;
+import it.unisa.theneverendingrun.models.Spawnable;
 import it.unisa.theneverendingrun.models.background.AbstractScrollingBackground;
 import it.unisa.theneverendingrun.models.background.ForestScrollingBackground;
+import it.unisa.theneverendingrun.models.enemy.Wolf;
 import it.unisa.theneverendingrun.models.hero.ForestHero;
 import it.unisa.theneverendingrun.models.hero.Hero;
+import it.unisa.theneverendingrun.models.obstacles.JumpableObstacle;
+import it.unisa.theneverendingrun.models.obstacles.JumpableSlidableObstacle;
+import it.unisa.theneverendingrun.models.obstacles.SlidableObstacle;
 
 public class ForestFactory implements GameFactory {
 
@@ -33,5 +38,21 @@ public class ForestFactory implements GameFactory {
     @Override
     public Hero createHero() {
         return hero;
+    }
+
+    public Wolf createWolf() {
+        return new Wolf(hero.getWidth() * 1.5f, hero.getHeight() * 0.8f);
+    }
+
+    public JumpableObstacle createJumpableObstacle() {
+        return new JumpableObstacle((float) hero.getJumpMaxElevation(), hero.getWidth());
+    }
+
+    public SlidableObstacle createSlidableObstacle() {
+        return new SlidableObstacle((float) hero.getMaxSlideRange() * 3); //fixme must be function of speed
+    }
+
+    public Spawnable createJumpableSlidableObstacle() {
+        return new JumpableSlidableObstacle((float) hero.getJumpMaxElevation(), (float) hero.getMaxSlideRange() * 3); //fixme idem
     }
 }
