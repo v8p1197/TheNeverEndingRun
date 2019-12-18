4ee6258173f2cc086f9b8ae333356ae886587743
vbaldi7 <v.baldi7@studenti.unisa.it>
2019-12-18 10:16:19 +0100

    FIXED EnemyAnimation interface implementation

--- Game/src/main/java/it/unisa/theneverendingrun/services/EnemyAnimation.java
+++ Game/src/main/java/it/unisa/theneverendingrun/services/EnemyAnimation.java
@@ -2,9 +2,8 @@ package it.unisa.theneverendingrun.services;
 
 
 import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public interface EnemyAnimation {
 
-    Animation<TextureRegion> setAnimation();
+    Animation setAnimation();
 }
