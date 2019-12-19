c2f86e088eb6ef395b1021364d49e0d23a9bbef9
Vincenzo Martusciello <vinz.mart@gmail.com>
2019-12-19 18:34:04 +0100

    Cleanup imports. Less lib dependency
    Signed-off-by: Vincenzo Martusciello <vinz.mart@gmail.com>

--- Game/src/main/java/it/unisa/theneverendingrun/models/background/impls/ForestBackground.java
+++ Game/src/main/java/it/unisa/theneverendingrun/models/background/impls/ForestBackground.java
@@ -1,6 +1,5 @@
 package it.unisa.theneverendingrun.models.background.impls;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import it.unisa.theneverendingrun.models.background.AbstractBackground;
 
