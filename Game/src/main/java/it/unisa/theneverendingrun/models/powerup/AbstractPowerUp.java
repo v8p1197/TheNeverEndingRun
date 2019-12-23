d94c35da4438f9a416f1d26da0f359368ce20481
andrewvali <58277048+andrewvali@users.noreply.github.com>
2019-12-23 16:44:50 +0100

    Added methods in class AbstractPowerUp and added power-up "Shield" in SpawnableTypes
    Signed-off-by: andrewvali <58277048+andrewvali@users.noreply.github.com>

--- Game/src/main/java/it/unisa/theneverendingrun/models/powerup/AbstractPowerUp.java
+++ Game/src/main/java/it/unisa/theneverendingrun/models/powerup/AbstractPowerUp.java
@@ -2,17 +2,34 @@ package it.unisa.theneverendingrun.models.powerup;
 
 import com.badlogic.gdx.graphics.Texture;
 import it.unisa.theneverendingrun.models.Spawnable;
+import it.unisa.theneverendingrun.models.SpawnableTypes;
 import it.unisa.theneverendingrun.models.hero.Hero;
 
 public class AbstractPowerUp extends Spawnable {
 
-    public AbstractPowerUp(Texture texture) {
+    private SpawnableTypes type;
+    private Boolean collected;
+
+    public AbstractPowerUp(Texture texture, SpawnableTypes type) {
         super(texture);
+        this.type = type;
+        this.collected = false;
+    }
+
+    public SpawnableTypes getType() {
+        return type;
+    }
+
+    public Boolean isCollected() {
+        return collected;
     }
 
     @Override
     public void beginCollision(Hero hero) {
-        
+        if (type == SpawnableTypes.Shield) {
+            // hero.addShield();
+        }
+        this.collected = true;
     }
 
     @Override
