05163845b6b0fee84e62045175a6864e1695bc87
v8p1197 <v.petrone6@studenti.unisa.it>
2019-12-25 11:42:03 +0100

    Now Fonts.load() is called by Assets.load()
    Signed-off-by: v8p1197 <v.petrone6@studenti.unisa.it>

--- Game/src/main/java/it/unisa/theneverendingrun/assets/Fonts.java
+++ Game/src/main/java/it/unisa/theneverendingrun/assets/Fonts.java
@@ -10,56 +10,78 @@ public class Fonts {
 
     public static BitmapFont scoreFont;
     public static BitmapFont meterFont;
+    public static BitmapFont endScreenTitleFont;
+    public static BitmapFont endScreenScoreFont;
 
-    public static void load() {
-        loadScoreFont();
-        loadMeterFont();
+    static void load() {
+        scoreFont = loadScoreFont();
+        meterFont = loadMeterFont();
+        endScreenTitleFont = loadEndScreenTitleFont();
+        endScreenScoreFont = loadEndScreenScoreFont();
     }
 
-    private static void loadScoreFont() {
+    private static BitmapFont loadEndScreenScoreFont() {
         //Generate a font object for font.ttf at size 40px
         FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
         parameter.size = 24;
         parameter.flip = false;
+        parameter.color = Color.WHITE;
+
+        return applyCommonParameters(parameter);
+    }
+
+    private static BitmapFont loadEndScreenTitleFont() {
+        //Generate a font object for font.ttf at size 40px
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 32;
+        parameter.flip = false;
+        parameter.color = Color.RED;
+
+        return applyCommonParameters(parameter);
+    }
+
+    private static BitmapFont loadScoreFont() {
+        //Generate a font object for font.ttf at size 40px
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 24;
+        parameter.flip = false;
+        parameter.color = Color.WHITE;
         parameter.borderColor = Color.BLACK;
         parameter.borderWidth = 2;
         parameter.shadowOffsetX = 2;
         parameter.shadowOffsetY = 2;
         parameter.shadowColor = Color.BLACK;
 
-        //The following settings allow the font to scale smoothly
-        parameter.magFilter = Texture.TextureFilter.Linear;
-        parameter.minFilter = Texture.TextureFilter.Linear;
-
-        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("ARCADE_N.ttf"));
-
-        scoreFont = generator.generateFont(parameter);
-        scoreFont.setUseIntegerPositions(false);
-
-        generator.dispose();
+        return applyCommonParameters(parameter);
     }
 
-    private static void loadMeterFont() {
+    private static BitmapFont loadMeterFont() {
         //Generate a font object for font.ttf at size 40px
         FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
         parameter.size = 18;
         parameter.flip = false;
+        parameter.color = Color.WHITE;
         parameter.borderColor = Color.BLACK;
         parameter.borderWidth = 2;
         parameter.shadowOffsetX = 1;
         parameter.shadowOffsetY = 1;
         parameter.shadowColor = Color.BLACK;
 
+        return applyCommonParameters(parameter);
+    }
 
+    private static BitmapFont applyCommonParameters(FreeTypeFontGenerator.FreeTypeFontParameter parameter) {
         //The following settings allow the font to scale smoothly
         parameter.magFilter = Texture.TextureFilter.Linear;
         parameter.minFilter = Texture.TextureFilter.Linear;
 
         FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("ARCADE_N.ttf"));
 
-        meterFont = generator.generateFont(parameter);
-        meterFont.setUseIntegerPositions(false);
+        var font = generator.generateFont(parameter);
+        font.setUseIntegerPositions(false);
 
         generator.dispose();
+
+        return font;
     }
 }
