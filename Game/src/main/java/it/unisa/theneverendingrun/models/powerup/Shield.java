c0d47e58b5ae6d91e6579baf97f13d5733ff8cf7
ltramuto <oraion123123>
2019-12-26 19:29:55 +0100

    Added class HandlingInput to listening the key button to move pg.

---
+++ Game/src/main/java/it/unisa/theneverendingrun/models/powerup/Shield.java
@@ -0,0 +1,15 @@
+package it.unisa.theneverendingrun.models.powerup;
+
+import com.badlogic.gdx.graphics.Texture;
+import it.unisa.theneverendingrun.models.SpawnableTypes;
+
+
+public class Shield extends AbstractPowerUp {
+
+    public Shield(Texture texture, float width, float height) {
+        super(texture, SpawnableTypes.Shield);
+        super.setSize(width, height);
+    }
+
+
+}
