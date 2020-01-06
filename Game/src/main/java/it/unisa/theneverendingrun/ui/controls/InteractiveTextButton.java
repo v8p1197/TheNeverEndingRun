8c10e6c0b347e5e09d97d30a21a4747a83277e97
Vincenzo Martusciello <vinz.mart@gmail.com>
2020-01-06 22:22:51 +0100

    Moved InteractiveTextButton to controls and removed ActionHandler
    Signed-off-by: Vincenzo Martusciello <vinz.mart@gmail.com>

--- Game/src/main/java/it/unisa/theneverendingrun/ui/controls/InteractiveTextButton.java
+++ Game/src/main/java/it/unisa/theneverendingrun/ui/controls/InteractiveTextButton.java
@@ -1,7 +1,8 @@
-package it.unisa.theneverendingrun.ui;
+package it.unisa.theneverendingrun.ui.controls;
 
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import it.unisa.theneverendingrun.ui.Actionable;
 
 public class InteractiveTextButton extends TextButton {
 
