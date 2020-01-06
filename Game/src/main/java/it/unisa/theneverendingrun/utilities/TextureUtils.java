bc3f11acc27d5fda31574262f386cc15976274f9
Vincenzo Martusciello <vinz.mart@gmail.com>
2020-01-06 22:23:30 +0100

    Add some check in textureUtils toVector method
    Signed-off-by: Vincenzo Martusciello <vinz.mart@gmail.com>

--- Game/src/main/java/it/unisa/theneverendingrun/utilities/TextureUtils.java
+++ Game/src/main/java/it/unisa/theneverendingrun/utilities/TextureUtils.java
@@ -6,6 +6,13 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 public final class TextureUtils {
 
     public static TextureRegion[] toVector(String path, String extension, int size) {
+        if (path == null) throw new NullPointerException("path is null");
+        if (path.isEmpty()) throw new IllegalArgumentException("path is not valid");
+
+        if (extension == null) throw new NullPointerException("extension is null");
+
+        if (size < 0) throw new IllegalArgumentException("size is not valid");
+
         var regions = new TextureRegion[size];
 
         for(int i = 0; i < size; ++i)
